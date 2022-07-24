36730097174a61b737d0326092e3155697592942
laurenp305 <lamor1800@gmail.com>
2022-07-24 12:38:01 -0400

    added endpoints to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
 //WHEN user inputs city
 //THEN list of flights show up and weather details for that location
+
+// Real-Time Flights
+
+
+const APIKey = cd340a575bd3d2c4abb310150d155c79,
+const queryURL = "https://api.aviationstack.com/v1/flights" 
++ cityName + "&appid=" + APIKey;
+    
+// // optional parameters: 
+
+//     & limit = 100
+//     & offset = 0
+//     & callback = MY_CALLBACK
+//     // more parameters available 
